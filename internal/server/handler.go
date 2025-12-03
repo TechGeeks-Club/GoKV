1140e20623b840633c465fef138d65da2b92f2f8
Amar A.J Bouakaz <amarbouakaz91@gmail.com>
2025-12-04 00:37:34 +0100

    refactor: streamline connection handling with buffered I/O

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -1,20 +1,22 @@
 
 package server
 
+import(
+	"bufio"
+)
+
 func HandleConnection(conn net.Conn){
 	defer conn.Close()
-	bufferLen := 4096
-	isFullBuffer := false
-    buffer := make([]byte, bufferLen)
+
+	r := bufio.NewReader(conn)
+    w := bufio.NewWriter(conn)
+
 	for {
-		n, err := conn.Read(buffer)
         if err != nil {
             fmt.Println("Error reading:", err)
             return
         }
-		if n == bufferLen {
-			isFullBuffer = true
-		}
+
 		// TODO : parse , (if there is an error in parsing that can mean that this is a part of the prev request (if the prev request ends with full buff))
 		// TODO : receive
 	}
